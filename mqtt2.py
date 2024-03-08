e87a6facde24c029703f8f61e694d7cbb2704da8
2014.felixchang <2014.felixchang@gmail.com>
2024-03-08 12:25:42 +0800

    Added commends for mqtt2.py

--- mqtt2.py
+++ mqtt2.py
@@ -47,9 +47,11 @@ class MQTTClient:
         # Clean up
         # GPIO.cleanup()
 
+    # Method to publish json with topic to broker
     def publish(self, topic, message):
         self.mqttc.publish(topic, json.dumps(message))
 
+    # Read temperature and humidity from sensor and publishes the data
     def readTempAndHumid(self, topic):
         try:
             temperature_c = dht_device.temperature
@@ -60,16 +62,24 @@ class MQTTClient:
             print("Temp:{:.1f} C / {:.1f} F    Humidity: {}%".format(temperature_c, temperature_f, humidity))
             # Publish the moisture level
             self.publish(topic, {'temp': "{:.2f}".format(temperature_c), 'humid':"{}%".format(humidity)})
+
+            # FOR TESTING (HARD CODED DATA, for work WITHOUT sensors)
+            # print("Temp:{:.1f} C / {:.1f} F    Humidity: {}%".format(30.50, 86.9, 55.55))
+            # self.publish(topic, {'temp': "{:.2f}".format(30.50), 'humid':"{}%".format(55.55)})
+
         except RuntimeError as err:
             print(err.args[0])
 
 
 if __name__ == "__main__":
-    # Usage
-    client = MQTTClient("test.mosquitto.org", 1883)#actual implementation use our network for now test with public broker
+    # Main method
+    # Replace "test.mosquitto.org" with the Broker IP such as "192.xxx.xxx.xxx" or wtv is the ip
+    client = MQTTClient("test.mosquitto.org", 1883)
+
     client.start()
     try:
         while True:
+            # Topic used is "home/temp", able to change depending on topic needed
             client.readTempAndHumid("home/temp")
             time.sleep(1)  # Delay for 1 second before reading again
     except KeyboardInterrupt:
