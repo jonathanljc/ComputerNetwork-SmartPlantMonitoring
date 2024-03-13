e8e56205c8c60c6f8369cd5f3349034b737de4fe
James <146323477+jpg2301136@users.noreply.github.com>
2024-03-13 22:04:43 +0800

    removed notify_clients and formatting

--- Assignment2/server2.py
+++ Assignment2/server2.py
@@ -39,11 +39,6 @@ class Server:
             else:
                 return username
 
-    # FOR TEAM: I realised this method isn't called at all
-    def notify_clients(self, message):
-        for client in self.client_sockets:
-            client.sendall(message.encode("utf-8"))
-
     # Runs the server
     def run(self):
         # Calls the start() method to start the server, if it is not already running
@@ -77,7 +72,12 @@ class Server:
             # Creates and starts a thread to handle the client's actions
             client_thread = threading.Thread(
                 target=handle_client,
-                args=(client_socket, self.client_sockets, self.client_names, self.groups),
+                args=(
+                    client_socket,
+                    self.client_sockets,
+                    self.client_names,
+                    self.groups,
+                ),
             )
             self.threads.append(client_thread)
             client_thread.start()
@@ -85,6 +85,7 @@ class Server:
         # by the way, this code is never technically reached
         self.server_socket.close()
         print("Server closed.")
+
 
 # Method to handle a client quitting the chat
 # Removes name from list of users and notifies all users that this user has left
@@ -95,11 +96,13 @@ def handle_quit(client_socket, clients, client_names):
     for client in clients:
         client.sendall(f"[{exiting_username}]: {message}".encode("utf-8"))
 
+
 # Method to send client the list of connected users
 def handle_names(client_socket, client_names):
     names = ", ".join(client_names.values())
     client_socket.sendall(f"[Connected users: {names}]".encode("utf-8"))
     print(f"[{client_names[client_socket]} requested user list]")
+
 
 # Method to take client's personal message and send to the targeted user
 def handle_personal_message(client_socket, client_names, message):
@@ -113,6 +116,7 @@ def handle_personal_message(client_socket, client_names, message):
             client_socket.sendall(msg_fmt.encode("utf-8"))
             print(f"[{client_names[client_socket]} -> {recipient_username}]")
             return
+
 
 # Method for client to setup a group chat
 def handle_group_setup(client_socket, client_names, groups, message):
@@ -133,13 +137,17 @@ def handle_group_setup(client_socket, client_names, groups, message):
 
         # Check if the group name already exists
         if group_name in groups:
-            client_socket.sendall(f"[Group '{group_name}' already exists.]".encode("utf-8"))
+            client_socket.sendall(
+                f"[Group '{group_name}' already exists.]".encode("utf-8")
+            )
             return
 
         # Check if all users in the group exist
         for username in users_list:
             if username not in client_names.values():
-                client_socket.sendall(f"[User '{username}' does not exist.]".encode("utf-8"))
+                client_socket.sendall(
+                    f"[User '{username}' does not exist.]".encode("utf-8")
+                )
                 return
 
         # Create the group and add users to it
@@ -162,6 +170,7 @@ def handle_group_setup(client_socket, client_names, groups, message):
 
     except Exception as e:
         client_socket.sendall(f"[Error setting up group: {e}]".encode("utf-8"))
+
 
 # Method for client to send a message in the group chat
 def handle_group_send(client_socket, client_names, groups, message):
@@ -188,32 +197,32 @@ def handle_group_send(client_socket, client_names, groups, message):
 
         # Check if client is inside the group
         if sender_username not in groups[group_name]:
-            client_socket.sendall("[You are not a member of this group.]".encode("utf-8"))
+            client_socket.sendall(
+                "[You are not a member of this group.]".encode("utf-8")
+            )
             return
 
         # Sends message to everyone in the group
         for username in groups[group_name]:
             if username != sender_username:
                 user_socket = [
-                    socket
-                    for socket, name in client_names.items()
-                    if name == username
+                    socket for socket, name in client_names.items() if name == username
                 ][0]
                 msg_fmt = f"[{sender_username} (group {group_name})]: {send_message}"
                 user_socket.sendall(msg_fmt.encode("utf-8"))
     except Exception as e:
         client_socket.sendall(f"[Error sending group message: {e}]".encode("utf-8"))
 
+
 # FOR TEAM: this method isn't called at all
-def send_group_setup_notification(
-    client_sockets, client_names, group_name, users_list
-):
+def send_group_setup_notification(client_sockets, client_names, group_name, users_list):
     for username in users_list:
         for socket, name in client_names.items():
             if name == username:
                 print(f"Sending group setup notification to {name}")
                 notification = f"[{username} have been added to the {group_name} group]"
                 socket.sendall(notification.encode("utf-8"))
+
 
 # Method for client deleting a group
 def handle_group_delete(client_socket, client_names, groups, message):
@@ -252,7 +261,6 @@ def handle_group_delete(client_socket, client_names, groups, message):
         client_socket.sendall(f"[Error deleting group: {e}]".encode("utf-8"))
 
 
-
 # Method for client leaving a group
 def handle_group_leave(client_socket, client_names, groups, message):
     try:
@@ -275,7 +283,9 @@ def handle_group_leave(client_socket, client_names, groups, message):
         # Check if user is part of this group
         sender_username = client_names[client_socket]
         if sender_username not in groups[group_name]:
-            client_socket.sendall("[You are not a member of this group.]".encode("utf-8"))
+            client_socket.sendall(
+                "[You are not a member of this group.]".encode("utf-8")
+            )
             return
 
         # Removes user from the group
@@ -291,6 +301,7 @@ def handle_group_leave(client_socket, client_names, groups, message):
         )
     except Exception as e:
         client_socket.sendall(f"[Error leaving group: {e}]".encode("utf-8"))
+
 
 # Methods that handles all the client's actions
 def handle_client(client_socket, clients, client_names, groups):
@@ -318,15 +329,15 @@ def handle_client(client_socket, clients, client_names, groups):
             # If message is "@group send",calls handle_group_send() to send message to group
             elif stripped.startswith("@group send"):
                 handle_group_send(client_socket, client_names, groups, stripped)
-            
+
             # If message is "@group delete",calls handle_group_delete() to delete group
             elif stripped.startswith("@group delete"):
                 handle_group_delete(client_socket, client_names, groups, stripped)
-            
+
             # If message is "@group leave",calls handle_group_leave() to make the client leave the group
             elif stripped.startswith("@group leave"):
                 handle_group_leave(client_socket, client_names, groups, stripped)
-            
+
             # If message is "@xxxx",calls handle_personal_message() to help client send a private message to target
             elif stripped.startswith("@"):
                 handle_personal_message(client_socket, client_names, message)
